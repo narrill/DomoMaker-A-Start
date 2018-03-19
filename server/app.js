51c52cc41c38bb275c87a6a0365046865f142df4
Mike Teixeira <miketeixeira95@gmail.com>
2018-03-19 13:17:32 -0400

    linter

--- server/app.js
+++ server/app.js
@@ -12,10 +12,10 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
-	if(err) {
-		console.log('Could not connect to database');
-		throw err;
-	}
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 const router = require('./router.js');
@@ -25,7 +25,7 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true,
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -35,8 +35,8 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-	if(err) {
-		throw(err);
-	}
-	console.log(`Listening on port ${port}`);
+  if (err) {
+    throw (err);
+  }
+  console.log(`Listening on port ${port}`);
 });
--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -9,31 +9,37 @@ const signupPage = (req, res) => {
 const logout = (req, res) => {
   res.redirect('/');
 };
-const login = (req, res) => {
+const login = (request, response) => {
+  const req = request;
+  const res = response;
+
   const username = `${req.body.username}`;
   const password = `${req.body.pass}`;
 
-  if(!username || !password) {
+  if (!username || !password) {
     return res.status(400).json({ error: 'RAWR! All fields are required' });
   }
 
   return Account.AccountModel.authenticate(username, password, (err, account) => {
-    if(err || !account) {
+    if (err || !account) {
       return res.status(400).json({ error: 'RAWR! WRONG USER NAME OR PASSWORD!' });
     }
     return res.json({ redirect: '/maker' });
   });
 };
-const signup = (req, res) => {
+const signup = (request, response) => {
+  const req = request;
+  const res = response;
+
   req.body.username = `${req.body.username}`;
   req.body.pass = `${req.body.pass}`;
   req.body.pass2 = `${req.body.pass2}`;
 
-  if(!req.body.username || !req.body.pass || !req.body.pass2) {
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
     return res.status(400).json({ error: 'RAWR! All fields are required' });
   }
 
-  if(req.body.pass !== req.body.pass2) {
+  if (req.body.pass !== req.body.pass2) {
     return res.status(400).json({ error: 'RAWR! Passwords do not match' });
   }
 
@@ -41,14 +47,14 @@ const signup = (req, res) => {
     const accountData = {
       username: req.body.username,
       salt,
-      password: hash
+      password: hash,
     };
     const newAccount = new Account.AccountModel(accountData);
     const savePromise = newAccount.save();
     savePromise.then(() => res.json({ redirect: '/maker' }));
     savePromise.catch((err) => {
       console.log(err);
-      if(err.code === 11000) {
+      if (err.code === 11000) {
         return res.status(400).json({ error: 'Username already in use' });
       }
       return res.status(400).json({ error: 'An error occurred' });
